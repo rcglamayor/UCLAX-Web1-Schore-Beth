e97f7772afcce123d0481a180cc04d7cc5bb3a26
Beth Schore <betro1@gmail.com>
2023-01-23 19:14:45 -0800

    S2 Sun and Moon

---
+++ src/SunAndMoon/SunAndMoon.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import sunSrc from './images/sun.png';
+import moonSrc from './images/moon.png';
+
+const SunAndMoon = () => {
+
+    // JavaScript
+
+    const [imgSrc, imgSrcUpdate] = useState(sunSrc);
+
+    const changeToMoon = () => {
+        // console.log('MOON');
+        imgSrcUpdate(moonSrc);
+    }
+
+    const changeToSun = () => {
+        // console.log('SUN');
+        imgSrcUpdate(sunSrc);
+    }
+
+    // HTML
+    return (
+        <SunAndMoonStyled>
+            <h2>Sun and Moon</h2>
+            <img src={imgSrc} onMouseEnter={ changeToMoon } onMouseLeave={ changeToSun } />
+        </SunAndMoonStyled>
+)
+}
+
+export default SunAndMoon;
+
+const SunAndMoonStyled = styled.div`
+    text-align: center;
+
+    h2 {
+        font-size: 50px;
+        color: teal;
+    }
+
+    img {
+        border: solid 20px teal;
+        margin: 30px;
+        padding: 20px;
+        background-color: #0c3b3b;
+    }
+`;

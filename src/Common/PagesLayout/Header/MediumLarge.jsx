e1472c30ebe5cf480a8fc81d9b795be3aa976f56
Beth Schore <betro1@gmail.com>
2023-02-13 18:49:05 -0800

    Add Hamburger icon

--- src/Common/PagesLayout/Header/MediumLarge.jsx
+++ src/Common/PagesLayout/Header/MediumLarge.jsx
@@ -11,7 +11,7 @@ const MediumLarge = () => {
     return (
         <MediumLargeStyled className='MediumLarge'>
             <Inset>
-                <div className="inset">
+                <div className="row">
                     <div className="logo">
                         <SiteLogo />
                     </div>
@@ -36,42 +36,54 @@ export default MediumLarge;
 const MediumLargeStyled = styled.div`
     color: white;
 
-.logo {
-    .SiteLogo {
-        width: 25%;
-        margin: auto;
-        padding: 20px 0px;
-    }
-}
+    .row {
+        display: grid;
+        grid-template-columns: 25% 75%;
 
-nav.mainMenu {
-    text-align: center;
-        a {
-            width: 80px;
-            display: inline-block;
-
-            line-height: 30px;
-            margin: 0px 0px 20px 5px;
-            border-bottom: solid 3px #84d4d4;
-
-            text-align: center;
-            font-size: 12px;
-            opacity: 0.8;
-            color: #84d4d4;
-            font-weight: bold;
-            text-decoration: none;
-            text-transform: uppercase;
-
-            &:hover,
-            &:active,
-            &:focus {
-                opacity: 1;
-            }
-
-            &.active {
-                color: white;
-                border-bottom-color: white;
-            }
+        .logo {  }
+        .menus {
+            display: flex;
+            justify-content: flex-end;
+            align-items: flex-end;
         }
-}
+    }
+
+    .logo {
+        .SiteLogo {
+            width: 80%;
+            margin: auto;
+            padding: 20px 0px;
+        }
+    }
+
+    nav.mainMenu {
+        text-align: center;
+            a {
+                width: 80px;
+                display: inline-block;
+
+                line-height: 30px;
+                margin: 0px 0px 20px 5px;
+                border-bottom: solid 3px #84d4d4;
+
+                text-align: center;
+                font-size: 12px;
+                opacity: 0.8;
+                color: #84d4d4;
+                font-weight: bold;
+                text-decoration: none;
+                text-transform: uppercase;
+
+                &:hover,
+                &:active,
+                &:focus {
+                    opacity: 1;
+                }
+
+                &.active {
+                    color: white;
+                    border-bottom-color: white;
+                }
+            }
+    }
 `;
--- src/Common/PagesLayout/Header/Small.jsx
+++ src/Common/PagesLayout/Header/Small.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+/* Components ---------------------------*/
+import SiteLogo from '../SiteLogo/SiteLogo.jsx';
+import Hamburger from './Hamburger.jsx';
 
 const Small = () => {
 
+    const [isMenuShowing, isMenuShowingUpdate] = useState(false);
+
+    const toggleMenu = () => {
+        isMenuShowingUpdate(!isMenuShowing);
+    }
+
+    const hideMenu = () => {
+        isMenuShowingUpdate(false);
+    }
+
+    useEffect(() => {
+        if (isMenuShowing) {
+            document.body.classList.add('modal-open');
+        } else {
+            document.body.classList.remove('modal-open');
+        }
+
+        return () => {
+            document.body.classList.remove('modal-open');
+        }
+
+    },[isMenuShowing]);
+
     return (
         <SmallStyled className='Small'>
-            Small
+            <SiteLogo />
+
+            <Hamburger toggleMenu={toggleMenu} />
+
+            {isMenuShowing && (
+                <nav className="mainMenu">
+                    <NavLink onClick={ hideMenu } to={'/'} end>
+                        Home
+                    </NavLink>
+                    <NavLink onClick={ hideMenu } to={'/staff'}>Staff</NavLink>
+                    <NavLink onClick={ hideMenu } to={'/contact'}>Contact</NavLink>
+                    <NavLink onClick={ hideMenu } to={'/course-work'}>Course</NavLink>
+                </nav>
+            )}
         </SmallStyled>
     );
 }
@@ -13,5 +55,41 @@ const Small = () => {
 export default Small;
 
 const SmallStyled = styled.div`
+    color: #fff;
 
+    display: flex;
+    justify-content: center;
+
+    .SiteLogo {
+        height: 300px;
+        padding: 20px;
+        max-width: 330px;
+    }
+
+    .mainMenu {
+        position: fixed;
+
+        left: 0px;
+        top: 300px;
+        right: 0px;
+        bottom: 0px;
+
+        background-color: #034444;
+
+        a {
+            display: block;
+
+            padding: 10px;
+            border-bottom: 1px solid #999999;
+
+            color: #fff;
+            text-decoration: none;
+
+            &:hover,
+            &:active,
+            &:focus {
+                background-color: #043434;
+            }
+        }
+    }
 `;
